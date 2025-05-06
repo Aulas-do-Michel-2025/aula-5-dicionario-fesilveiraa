28951bc048ae495549cb1313475b575e45d81390
fesilveiraa <fesilveira23@gmail.com>
2025-05-06 16:21:25 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -1,47 +1,3 @@
-"""
-#### Exercício 1 - Buscando Informações de Genes
-
-Você tem um dicionário onde:
-
-A chave é o nome do gene.
-
-O valor é o tamanho do gene (em pares de base).
-
-Tarefa:
-
-Peça para o usuário digitar o nome de um gene.
-
-Depois, procure no dicionário:
-
-Se o gene existir, imprima: "O tamanho do gene [NOME] é [TAMANHO] pb."
-
-Se não existir, imprima: "Gene não encontrado."
-
-------------
-
-Exemplo:
-
-Entrada:
-
-Digite o nome do gene: BRCA1
-
-Saída:
-
-O tamanho do gene BRCA1 é [TAMANHO] pb."
-
-------------
-
-Exemplo:
-
-Entrada:
-
-Digite o nome do gene: BRCA3
-
-Saída:
-
-Gene não encontrado.
-"""
-
 genes = {
     "BRCA1": 81188,
     "TP53": 19054,
@@ -53,6 +9,11 @@ genes = {
     "DMD": 2400000,
     "HTT": 169451,
     "FMR1": 38000,
-}
+} 
 
-# Criar seu código a partir daqui
+nome_gene = input ("Digite o nome do gene:  ")
+
+if nome_gene in genes:
+    print (f"O tamanho gene {nome_gene} é {genes [nome_gene]}")
+else:
+    print ("Gene não encontrado.") 
